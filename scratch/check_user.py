67bae592a98bdd7c16e61d8f3a6bb488d790af09
Emre ÇAVDAR <emrecavdar@gmail.com>
2026-04-20 12:21:22 +0300

    chore: GP-11 - untrack scratch/ debug files

--- scratch/check_user.py
+++
@@ -1,9 +0,0 @@
-from database.connection import get_engine
-import pandas as pd
-from sqlalchemy import text
-
-engine = get_engine()
-with engine.connect() as conn:
-    res = conn.execute(text("SELECT ad_soyad, kullanici_adi, rol, durum FROM personel WHERE ad_soyad LIKE '%Emre%' OR kullanici_adi LIKE '%Emre%';"))
-    df = pd.DataFrame(res.fetchall(), columns=res.keys())
-    print(df.to_string())
